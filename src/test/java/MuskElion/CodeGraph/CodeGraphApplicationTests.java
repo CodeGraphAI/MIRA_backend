fbdc641ead240f6b6b9aaf5f47f59f39f1558b80
LeeHyunWoo <hyoun0824@naver.com>
2025-07-06 23:01:46 +0900

    docs : 프로젝트 추가

---
+++ src/test/java/MuskElion/CodeGraph/CodeGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package MuskElion.CodeGraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeGraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
