3b1a20083c8fd31b62993e715437d648abe9acb8
LeeHyunWoo <hyoun0824@naver.com>
2025-07-10 18:55:09 +0900

    docs : swagger 의존성 추가, 명세화

--- src/main/java/MuskElion/CodeGraph/gemini/controller/GeminiController.java
+++ src/main/java/MuskElion/CodeGraph/gemini/controller/GeminiController.java
@@ -3,6 +3,10 @@ package MuskElion.CodeGraph.gemini.controller;
 import MuskElion.CodeGraph.gemini.dto.QueryRequestDTO;
 import MuskElion.CodeGraph.gemini.dto.QueryResponseDTO;
 import MuskElion.CodeGraph.gemini.service.GeminiService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +21,18 @@ public class GeminiController {
 
     private final GeminiService service;
 
+    @Operation(
+            summary = "질문하기",
+            description = "질문 텍스트를 입력하면 Gemini AI의 답변을 반환합니다."
+                    ,
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "성공",
+                            content = @Content(schema = @Schema(implementation = QueryResponseDTO.class))
+                    )
+            }
+    )
     @PostMapping("/ask")
     public ResponseEntity<QueryResponseDTO> askGemini(@RequestBody QueryRequestDTO requestDTO){
         String answer = service.getCompletion(requestDTO.getText());
